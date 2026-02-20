394fbe9ed7422510b6c55269aca4ba6bb6f86a80
Sonu H N <sonuhn0011@gmail.com>
2026-02-20 21:44:19 +0530

    Added light and dark mode

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        toggleBtn.textContent = "☀️";
+    } else {
+        toggleBtn.textContent = "🌙";
+    }
+});
