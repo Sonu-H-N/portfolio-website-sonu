db656e6dfe04af6e7f1be91bc9d956aab0dd75cc
Sonu H N <sonuhn0011@gmail.com>
2026-02-21 18:38:35 +0530

    Added theme animation and save preference

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const toggleBtn = document.getElementById("theme-toggle");
 
+// Load saved theme
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light-mode");
+    toggleBtn.textContent = "☀️";
+}
+
+// Toggle theme
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
 
     if (document.body.classList.contains("light-mode")) {
         toggleBtn.textContent = "☀️";
+        localStorage.setItem("theme", "light");
     } else {
         toggleBtn.textContent = "🌙";
+        localStorage.setItem("theme", "dark");
     }
 });
