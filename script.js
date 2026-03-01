c422496c138e01d3b1667e68e0fe729e1ab2f871
Sonu H N <sonuhn0011@gmail.com>
2026-03-01 11:22:07 +0530

    Added hamburger mobile menu

--- script.js
+++ script.js
@@ -18,3 +18,9 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
